69645da1f1ba40a593ab18f60a5dc71d26a2e237
Nicolas Widart <n.widart@gmail.com>
2014-10-22 20:13:19 +0200

    Add the corresponding interface method

--- Repositories/SettingRepository.php
+++ Repositories/SettingRepository.php
@@ -32,4 +32,11 @@ interface SettingRepository extends BaseRepository
      * @return mixed
      */
     public function savedModuleSettings($module);
+
+    /**
+     * Find settings by module name
+     * @param string $module
+     * @return mixed
+     */
+    public function findByModule($module);
 }
