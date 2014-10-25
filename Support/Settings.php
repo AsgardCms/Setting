9fc82089d17a9cb165cfea5f3cfc682ebd93d2d4
Nicolas Widart <n.widart@gmail.com>
2014-10-25 11:28:47 +0200

    Cleaning up the settings class

--- Support/Settings.php
+++ Support/Settings.php
@@ -1,6 +1,5 @@
 <?php namespace Modules\Setting\Support;
 
-use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 use Modules\Setting\Repositories\SettingRepository;
 
 class Settings
@@ -21,17 +20,15 @@ class Settings
     /**
      * Getting the setting
      * @param $setting
+     * @param null $locale
      * @return mixed
      */
-    public function get($setting)
+    public function get($setting, $locale = null)
     {
         $setting = $this->setting->get($setting);
 
         if ($setting->isTranslatable) {
-            foreach (LaravelLocalization::getSupportedLocales() as $locale => $language) {
-                $settings[$locale] = $setting->translate($locale)->value;
-            }
-            return $settings;
+            return $setting->translate($locale)->value;
         }
 
         return $setting->plainValue;
