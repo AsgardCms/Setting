20d1cdb3d658f785e17d51836fc26757c4078b32
Nicolas Widart <n.widart@gmail.com>
2014-11-08 18:16:24 +0100

    Fixing setting button

--- Resources/views/admin/module-settings.blade.php
+++ Resources/views/admin/module-settings.blade.php
@@ -82,7 +82,7 @@
         </div>
         <?php endif; ?>
         <div class="box-footer">
-            <button type="submit" class="btn btn-primary btn-flat">{{ trans('core::core.button.create') }}</button>
+            <button type="submit" class="btn btn-primary btn-flat">{{ trans('core::core.button.update') }}</button>
             <a class="btn btn-danger pull-right btn-flat" href="{{ URL::route('dashboard.setting.index')}}"><i class="fa fa-times"></i> {{ trans('core::core.button.cancel') }}</a>
         </div>
     </div>
