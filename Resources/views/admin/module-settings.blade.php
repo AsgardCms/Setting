cc165ce1ccd8dc24749db69625dd909d417f7c7e
Nicolas Widart <n.widart@gmail.com>
2014-11-19 20:48:40 +0100

    Extract the tab headers

--- Resources/views/admin/module-settings.blade.php
+++ Resources/views/admin/module-settings.blade.php
@@ -50,13 +50,7 @@
             <div class="box-body">
                 <div class="nav-tabs-custom">
                     <ul class="nav nav-tabs">
-                        <?php $i = 0; ?>
-                        <?php foreach(LaravelLocalization::getSupportedLocales() as $locale => $language): ?>
-                            <?php $i++; ?>
-                            <li class="{{ App::getLocale() == $locale ? 'active' : '' }}">
-                                <a href="#tab_{{ $i }}" data-toggle="tab">{{ trans('core::core.tab.'. strtolower($language['name'])) }}</a>
-                            </li>
-                        <?php endforeach; ?>
+                        @include('core::partials.form-tab-headers')
                     </ul>
                     <div class="tab-content">
                         <?php $i = 0; ?>
