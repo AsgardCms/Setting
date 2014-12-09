04113098e13b9e633196fbae04df8e4e4cefaf6f
Nicolas Widart <n.widart@gmail.com>
2014-12-09 19:39:41 +0100

    Adding menu seed file

--- Database/Seeders/SettingDatabaseSeeder.php
+++ Database/Seeders/SettingDatabaseSeeder.php
@@ -2,9 +2,20 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+use Modules\Setting\Repositories\SettingRepository;
 
 class SettingDatabaseSeeder extends Seeder
 {
+
+    /**
+     * @var SettingRepository
+     */
+    private $setting;
+
+    public function __construct(SettingRepository $setting)
+    {
+        $this->setting = $setting;
+    }
 
     /**
      * Run the database seeds.
@@ -14,6 +25,14 @@ class SettingDatabaseSeeder extends Seeder
     public function run()
     {
         Model::unguard();
+
+        $data = [
+            'name' => 'core::template',
+            'plainValue' => 'Demo',
+            'isTranslatable' => false,
+        ];
+
+        $this->setting->create($data);
     }
 
 }
