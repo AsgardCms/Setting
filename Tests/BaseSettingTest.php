8e11255d1d7e6c99a83ca75866f7a92d9d40dc40
Nicolas Widart <n.widart@gmail.com>
2014-12-29 19:46:32 +0100

    Add a base setting test case

---
+++ Tests/BaseSettingTest.php
@@ -0,0 +1,22 @@
+<?php namespace Modules\Setting\Tests;
+
+use Modules\Core\Tests\BaseTestCase;
+
+abstract class BaseSettingTest extends BaseTestCase
+{
+    /**
+     * @var \Modules\Setting\Repositories\Eloquent\EloquentSettingRepository
+     */
+    protected $settingRepository;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        /** @var \Illuminate\Console\Application $artisan */
+        $artisan = $this->app->make('Illuminate\Console\Application');
+        $artisan->call('module:migrate', ['module' => 'Setting']);
+
+        $this->settingRepository = app('Modules\Setting\Repositories\Eloquent\EloquentSettingRepository');
+    }
+}
