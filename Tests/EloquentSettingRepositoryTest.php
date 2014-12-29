c41eb40bac558c3858ec59a6c9b66c53e4c9f38a
Nicolas Widart <n.widart@gmail.com>
2014-12-29 19:59:55 +0100

    Add test to create a translatable setting

---
+++ Tests/EloquentSettingRepositoryTest.php
@@ -0,0 +1,30 @@
+<?php namespace Modules\Setting\Tests;
+
+class EloquentSettingRepositoryTest extends BaseSettingTest
+{
+    public function setUp()
+    {
+        parent::setUp();
+    }
+
+    /** @test */
+    public function it_creates_translated_setting()
+    {
+        // Prepare
+        $data = [
+            'core::site-name' => [
+                'en' => 'AsgardCMS_en',
+                'fr' => 'AsgardCMS_fr',
+            ]
+        ];
+
+        // Run
+        $this->settingRepository->createOrUpdate($data);
+
+        // Assert
+        $setting = $this->settingRepository->find(1);
+        $this->assertEquals('core::site-name', $setting->name);
+        $this->assertEquals('AsgardCMS_en', $setting->translate('en')->value);
+        $this->assertEquals('AsgardCMS_fr', $setting->translate('fr')->value);
+    }
+}
