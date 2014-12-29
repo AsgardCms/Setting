19219e28c313e03a4bf19f037565b071bd00f34d
Nicolas Widart <n.widart@gmail.com>
2014-12-29 20:01:38 +0100

    Add test to create a plain setting

--- Tests/EloquentSettingRepositoryTest.php
+++ Tests/EloquentSettingRepositoryTest.php
@@ -27,4 +27,21 @@ class EloquentSettingRepositoryTest extends BaseSettingTest
         $this->assertEquals('AsgardCMS_en', $setting->translate('en')->value);
         $this->assertEquals('AsgardCMS_fr', $setting->translate('fr')->value);
     }
+
+    /** @test */
+    public function it_creates_plain_setting()
+    {
+        // Prepare
+        $data = [
+            'core::template' => 'asgard'
+        ];
+
+        // Run
+        $this->settingRepository->createOrUpdate($data);
+
+        // Assert
+        $setting = $this->settingRepository->find(1);
+        $this->assertEquals('core::template', $setting->name);
+        $this->assertEquals('asgard', $setting->plainValue);
+    }
 }
