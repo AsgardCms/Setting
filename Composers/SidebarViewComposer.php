6ac556fa09b215d9fddaa18435fba42910a242a1
Patrick Brouwers <patrickbrouwersfilm@gmail.com>
2015-02-16 19:06:11 +0100

    Update sidebar view composer

--- Composers/SidebarViewComposer.php
+++ Composers/SidebarViewComposer.php
@@ -9,14 +9,12 @@ class SidebarViewComposer extends BaseSidebarViewComposer
 {
     public function compose(View $view)
     {
-        $view->sidebar->group('Setting', function (SidebarGroup $group) {
-            $group->enabled = false;
-            $group->weight = 4;
+        $view->sidebar->group(trans('workshop::workshop.title'), function (SidebarGroup $group) {
 
-            $group->addItem('Setting', function (SidebarItem $item) {
-                $item->route('admin.setting.settings.index');
+            $group->addItem(trans('setting::settings.title.settings'), function (SidebarItem $item) {
                 $item->icon = 'fa fa-cog';
-                $item->name = 'Setting';
+                $item->weight = 50;
+                $item->route('admin.setting.settings.index');
                 $item->authorize(
                     $this->auth->hasAccess('settings.index')
                 );
