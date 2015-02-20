0966de9cca34e12cf7332e515a60728cd1535b59
Nicolas Widart <n.widart@gmail.com>
2015-02-20 14:39:39 +0100

    Try to add a base test case

--- Tests/BaseSettingTest.php
+++ Tests/BaseSettingTest.php
@@ -1,8 +1,8 @@
 <?php namespace Modules\Setting\Tests;
 
-use Modules\Core\Tests\BaseTestCase;
+use Orchestra\Testbench\TestCase;
 
-abstract class BaseSettingTest extends BaseTestCase
+abstract class BaseSettingTest extends TestCase
 {
     /**
      * @var \Modules\Setting\Repositories\SettingRepository
@@ -13,10 +13,50 @@ abstract class BaseSettingTest extends BaseTestCase
     {
         parent::setUp();
 
-        /** @var \Illuminate\Contracts\Console\Kernel $artisan */
-        $artisan = $this->app->make('Illuminate\Contracts\Console\Kernel');
-        $artisan->call('module:migrate', ['module' => 'Setting']);
+        $this->resetDatabase();
 
         $this->settingRepository = app('Modules\Setting\Repositories\SettingRepository');
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return ['Modules\Setting\Providers\SettingServiceProvider'];
+    }
+
+    protected function getEnvironmentSetUp($app)
+    {
+        $app['path.base'] = __DIR__ . '/..';
+        $app['config']->set('database.default', 'sqlite');
+        $app['config']->set('database.connections.sqlite', array(
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prefix' => '',
+        ));
+    }
+
+    protected function getPackageAliases($app)
+    {
+        return ['Eloquent' => 'Illuminate\Database\Eloquent\Model'];
+    }
+
+    private function resetDatabase()
+    {
+        // Relative to the testbench app folder: vendors/orchestra/testbench/src/fixture
+        $migrationsPath = 'Database/Migrations';
+        $artisan = $this->app->make('Illuminate\Contracts\Console\Kernel');
+        // Makes sure the migrations table is created
+        $artisan->call('migrate', [
+            '--database' => 'sqlite',
+            '--path'     => $migrationsPath,
+        ]);
+        // We empty all tables
+        $artisan->call('migrate:reset', [
+            '--database' => 'sqlite',
+        ]);
+        // Migrate
+        $artisan->call('migrate', [
+            '--database' => 'sqlite',
+            '--path'     => $migrationsPath,
+        ]);
     }
 }
