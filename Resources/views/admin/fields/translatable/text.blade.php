da662f4a5b68035817d9974ab78f15b09cd683e8
Nicolas Widart <n.widart@gmail.com>
2015-03-16 13:45:55 +0100

    Check for the object

--- Resources/views/admin/fields/translatable/text.blade.php
+++ Resources/views/admin/fields/translatable/text.blade.php
@@ -1,7 +1,9 @@
 <div class='form-group'>
     {!! Form::label($settingName . "[$lang]", trans($moduleInfo['description'])) !!}
     <?php if (isset($dbSettings[$settingName])): ?>
-        {!! Form::text($settingName . "[$lang]", Input::old($settingName . "[$lang]", $dbSettings[$settingName]->translate($lang)->value), ['class' => 'form-control', 'placeholder' => trans($moduleInfo['description'])]) !!}
+        <?php $object = $dbSettings[$settingName]->translate($lang); ?>
+        <?php $value = isset($object) ? $object->value : '' ?>
+        {!! Form::text($settingName . "[$lang]", Input::old($settingName . "[$lang]", $value), ['class' => 'form-control', 'placeholder' => trans($moduleInfo['description'])]) !!}
     <?php else: ?>
         {!! Form::text($settingName . "[$lang]", Input::old($settingName . "[$lang]"), ['class' => 'form-control', 'placeholder' => trans($moduleInfo['description'])]) !!}
     <?php endif; ?>
--- Resources/views/admin/fields/translatable/textarea.blade.php
+++ Resources/views/admin/fields/translatable/textarea.blade.php
@@ -1,7 +1,9 @@
 <div class='form-group'>
     {!! Form::label($settingName . "[$lang]", trans($moduleInfo['description'])) !!}
     <?php if (isset($dbSettings[$settingName])): ?>
-        {!! Form::textarea($settingName . "[$lang]", Input::old($settingName . "[$lang]", $dbSettings[$settingName]->translate($lang)->value), ['class' => 'form-control', 'placeholder' => trans($moduleInfo['description'])]) !!}
+        <?php $object = $dbSettings[$settingName]->translate($lang); ?>
+        <?php $value = isset($object) ? $object->value : '' ?>
+        {!! Form::textarea($settingName . "[$lang]", Input::old($settingName . "[$lang]", $value), ['class' => 'form-control', 'placeholder' => trans($moduleInfo['description'])]) !!}
     <?php else: ?>
         {!! Form::textarea($settingName . "[$lang]", Input::old($settingName . "[$lang]"), ['class' => 'form-control', 'placeholder' => trans($moduleInfo['description'])]) !!}
     <?php endif; ?>
