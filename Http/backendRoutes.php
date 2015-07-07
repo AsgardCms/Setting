fbbede3062278746feb6add9d1bb075447f8543e
Nicolas Widart <n.widart@gmail.com>
2015-07-07 20:44:19 +0200

    Use manual route definition over the route resource shortcut

--- Http/backendRoutes.php
+++ Http/backendRoutes.php
@@ -1,12 +1,7 @@
 <?php
 
-use Illuminate\Routing\Router;
-
-$router->group(['prefix' => '/setting'], function (Router $router) {
-    $router->get('settings/{module}', ['as' => 'dashboard.module.settings', 'uses' => 'SettingController@getModuleSettings']);
-
-    $router->resource('settings', 'SettingController', ['except' => ['show', 'edit', 'update', 'destroy', 'create'], 'names' => [
-        'index' => 'admin.setting.settings.index',
-        'store' => 'admin.setting.settings.store',
-    ]]);
+$router->group(['prefix' => '/setting'], function () {
+    get('settings/{module}', ['as' => 'dashboard.module.settings', 'uses' => 'SettingController@getModuleSettings']);
+    get('settings', ['as' => 'admin.setting.settings.index', 'uses' => 'SettingController@index']);
+    post('settings', ['as' => 'admin.setting.settings.store', 'uses' => 'SettingController@store']);
 });
