8e984cfd58ed427fec00f9758ec64bf26b4f386a
Nuno Ribeiro <nmpribeiro@gmail.com>
2015-08-20 15:24:17 +0000

    Added de locale and pt

---
+++ Resources/lang/de/settings.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'title' => [
+        'settings' => 'Einstellungen',
+        'general settings' => 'Allgemeine Einstellungen',
+        'module settings' => 'Moduleinstellungen',
+        'module name settings' => ':module Moduleinstellungen',
+    ],
+    'breadcrumb' => [
+        'settings' => 'Einstellungen',
+        'module settings' => ':module Moduleinstellungen',
+    ],
+];
