24fbd0da378bdf562eab70d7f0becb51b2542b01
Nicolas Widart <n.widart@gmail.com>
2015-11-27 15:33:09 +0100

    Removing manual iCheck inlcudes in views

--- Resources/views/admin/module-settings.blade.php
+++ Resources/views/admin/module-settings.blade.php
@@ -11,10 +11,6 @@
 </ol>
 @stop
 
-@section('styles')
-    {!! Theme::style('css/vendor/iCheck/flat/blue.css') !!}
-@stop
-
 @section('content')
 {!! Form::open(['route' => ['admin.setting.settings.store'], 'method' => 'post']) !!}
 <div class="row">
