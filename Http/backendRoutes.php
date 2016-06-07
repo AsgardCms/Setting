7e218a68fd0a49ddaa7e55c8ca5087a807c9c64f
Nicolas Widart <n.widart@gmail.com>
2016-06-07 21:32:57 +0200

    Using the $router variable instead of helpers

--- Http/backendRoutes.php
+++ Http/backendRoutes.php
@@ -1,7 +1,11 @@
 <?php
 
-$router->group(['prefix' => '/setting'], function () {
-    get('settings/{module}', ['as' => 'dashboard.module.settings', 'uses' => 'SettingController@getModuleSettings']);
-    get('settings', ['as' => 'admin.setting.settings.index', 'uses' => 'SettingController@index']);
-    post('settings', ['as' => 'admin.setting.settings.store', 'uses' => 'SettingController@store']);
+use Illuminate\Routing\Router;
+
+/** @var Router $router */
+
+$router->group(['prefix' => '/setting'], function (Router $router) {
+    $router->get('settings/{module}', ['as' => 'dashboard.module.settings', 'uses' => 'SettingController@getModuleSettings']);
+    $router->get('settings', ['as' => 'admin.setting.settings.index', 'uses' => 'SettingController@index']);
+    $router->post('settings', ['as' => 'admin.setting.settings.store', 'uses' => 'SettingController@store']);
 });
